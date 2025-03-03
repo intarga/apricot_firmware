889abfbd58febea848ad03ddbd2fcd8d807cd68c
Ingrid <git@ingrids.email>
2025-03-03 17:07:56 +0100

    move more things into info.json

--- rules.mk
+++ rules.mk
@@ -1,14 +1,1 @@
-# MCU name
 
-# Bootloader selection
-
-# Build Options
-# change yes to no to disable
-#
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
